fcbcd7b61dffd8abd6552672a18c3c4579270ec6
vigneshwara044 <vigneshwara044@gmail.com>
2019-04-09 16:19:27 +0530

    echo-server

--- Day_3/echo-server/src/main.rs
+++ Day_3/echo-server/src/main.rs
@@ -1,5 +1,4 @@
 extern crate tokio;
-extern crate futures;
 
 use tokio::io;
 use tokio::net::TcpListener;
@@ -10,5 +9,27 @@ fn main() {
     let addr = "127.0.0.1:6142".parse().unwrap();
     let listner = TcpListener::bind(&addr).unwrap();
 
-    let server = li
+    let server = listner.incoming().for_each(|socket| {
+
+        let (reader, writer) = socket.split();
+
+        let amount = io::copy(reader, writer);
+
+        let msg = amount.then( |result|{
+            match result {
+                Ok((amount,_, _)) => println!("wrote {} bytes", amount),
+                Err(e)            => println!("error: {}", e),  
+            }
+            Ok(())    
+        });
+
+        tokio::spawn(msg); 
+        Ok(())
+    })
+    .map_err(|err| {
+        println!("accept error = {:?}", err);
+    });
+    println!("server running on localhost:6142");
+
+    tokio::run(server);
 }
